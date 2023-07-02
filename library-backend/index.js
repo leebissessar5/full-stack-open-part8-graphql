84b1d3354490d7863f25e40efbee48a9d1bcadc4
leebissessar5 <leebissessar@gmail.com>
2023-07-02 16:12:24 -0400

    complete ex8.13-15

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,16 +1,14 @@
-const { ApolloServer } = require("@apollo/server");
+const { ApolloServer } = require('@apollo/server');
 require("dotenv").config();
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const { v1: uuid } = require("uuid");
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const Author = require('./models/author')
 const Book = require('./models/book');
-const book = require("./models/book");
 
 console.log("connecting to", MONGODB_URI);
 
@@ -22,89 +20,6 @@ mongoose
   .catch((error) => {
     console.log("error connection to MongoDB:", error.message);
   });
-
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-/*
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
 
 const typeDefs = `
   type Book {
@@ -178,6 +93,10 @@ const resolvers = {
       let author = await Author.findOne({ name: args.author });
       if (!author) {
         author = new Author({ name: args.author, born: null })
+        const authorValidationErrors = author.validateSync();
+        if (authorValidationErrors) {
+          throw new Error("Author validation failed");
+        }
         await author.save()
       }
       const book = new Book({
@@ -186,7 +105,10 @@ const resolvers = {
         published: args.published,
         genres: args.genres,
       });
-      console.log(book)
+      const bookValidationErrors = book.validateSync();
+      if (bookValidationErrors) {
+        throw new Error("book validation failed");
+      }
       await book.save();
       return book
     },
@@ -194,22 +116,15 @@ const resolvers = {
       const { name, setBornTo } = args
       const filter = { name: name };
       const author = await Author.findOne(filter)
-      try {
-        await Author.updateOne(
-          filter,
-          {
-            $set: {
-              ...author.document,
-              born: setBornTo,
-            },
+      await Author.updateOne(filter,
+        {
+          $set: {
+            ...author.document,
+            born: setBornTo,
           },
-          {}
-        );
-      } catch (e) {
-        throw new UserInputError(e.message, {
-          invalidArgs: args,
-        });
-      }
+        },
+        {}
+      );
       const updated = await Author.findOne(filter);
       const books = await Book.find({}).populate("author");
       updated.bookCount = books.filter((book) => book.author.name === updated.name).length
